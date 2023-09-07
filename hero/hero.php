cedf9ac50681c711e374360258e65e9b5ecd20e7
jandcreative <javi.diaz@jandcreative.com>
2023-09-07 10:36:11 +0200

    delete blocks

--- hero/hero.php
+++ hero/hero.php
@@ -8,7 +8,7 @@
     $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
  endif;
 
- $class_name = 'jda-hero';
+ $class_name = 'hero';
  if( !empty($block['className'] ) ):
     $class_name .= ' ' . esc_attr( $block['className'] );
  endif;
@@ -19,10 +19,12 @@ endif;
 
 
 // ACF Fields
-$title = get_field('jda_hero_title');
-$descripcion = get_field('jda_hero_description');
-$img = get_field('jda_hero_image');
+$title = get_field('hero_title');
+$descripcion = get_field('hero_description');
+$img = get_field('hero_image');
 ?>
+
+<section class="hero_home">
 
 <div <?php echo $anchor;?>class="<?php echo $class_name;
 
@@ -34,10 +36,10 @@ $img = get_field('jda_hero_image');
         <h1><?php echo $title; ?></h1>
           <p><?php echo $descripcion; ?></p>
           <div class="links">
-					<a class="button" ref="#">Get Started<div style="display:inline-flex;justify-content:center;align-items:center" class="kt-btn-svg-icon kt-btn-svg-icon-fas_chevron-right kt-btn-side-right"><svg style="display:inline-block;vertical-align:middle" viewBox="0 0 320 512" height="1em" width="1em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg></div></a>
+					<a class="button" ref="#">Check out our process<div style="display:inline-flex;justify-content:center;align-items:center" class="kt-btn-svg-icon kt-btn-svg-icon-fas_chevron-right kt-btn-side-right"><svg style="display:inline-block;vertical-align:middle" viewBox="0 0 320 512" height="1em" width="1em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg></div></a>
 					
-					<a class="button action" href="#" target="_blank">How it works<div style="display:inline-flex;justify-content:center;align-items:center" class="kt-btn-svg-icon kt-btn-svg-icon-fas_caret-right kt-btn-side-right"><svg style="display:inline-block;vertical-align:middle" viewBox="0 0 192 512" height="1em" width="1em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path></svg></div></a>
-
+					<!-- <a class="button action" href="#" target="_blank">How it works<div style="display:inline-flex;justify-content:center;align-items:center" class="kt-btn-svg-icon kt-btn-svg-icon-fas_caret-right kt-btn-side-right"><svg style="display:inline-block;vertical-align:middle" viewBox="0 0 192 512" height="1em" width="1em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path></svg></div></a>
+ -->
 			</div>
         
     </div>
@@ -48,5 +50,6 @@ $img = get_field('jda_hero_image');
     <?php endif;?>
         
 </div>
+               </section>
 
 
