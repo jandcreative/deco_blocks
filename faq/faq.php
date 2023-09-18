f9dbfaa5da9c5050cf4b1b232d79118cf764fab7
jandcreative <javi.diaz@jandcreative.com>
2023-09-18 19:47:16 +0200

    add block - faq

---
+++ faq/faq.php
@@ -0,0 +1,24 @@
+<section id="faq" class="faq">
+
+    <div class="title">
+        <h2>Frequently asked questions about programmatic SEO</h2>
+    </div>
+
+    <div class="container-faq">
+
+        <div class="question-box">
+            
+            <div class="item-faq"> 
+               
+                <h2 class="question">How do you implement programmatic SEO?</h2>
+                <span class="plus"> <img src="https://seinserigrafia.es/wp-content/uploads/2021/07/croix.svg"> </span>
+            </div>
+            
+            <div class="description-ask">
+                <p>La serigrafía es una técnica que permite la impresión sobre cualquier tipo de material, mediante la transferencia de tinta a través de una malla tensada en un marco.</p>
+            </div>
+        </div>
+
+    </div>
+
+</section>
