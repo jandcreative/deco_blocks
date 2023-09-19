4decbb2e1a94952d8b062f97b6be984d1bafd3d8
jandcreative <javi.diaz@jandcreative.com>
2023-09-19 15:59:32 +0200

    update block - goals

--- jda-acf-blocks.php
+++ jda-acf-blocks.php
@@ -1,19 +1,35 @@
 <?php
 
-/* Plugin Name: ACF Blocks */
+/* 
+  Plugin Name: ACF Blocks 
+  Description:  The developer tools panel for WordPress.
+  Version:      1.0
+*/
 
 add_action( 'init', 'jda_register_acf_blocks', 5 );
 
 function jda_register_acf_blocks() {
 
+	// Register Cabecera  block
+	register_block_type( __DIR__ . '/hero' );
+
+	// Register Cabecera  block
+	register_block_type( __DIR__ . '/cabecera' );
+
 	// Register Spacers block
 	register_block_type( __DIR__ . '/spacer' );
 
-	// Register Hero
-	register_block_type( __DIR__ . '/hero' );
-
 	// Register Brands block
 	register_block_type( __DIR__ . '/brands' );
+
+	// Register Result
+	register_block_type( __DIR__ . '/alpha' );
+
+	// Register Result
+	register_block_type( __DIR__ . '/alpha-process' );
+
+	// Register Result
+	register_block_type( __DIR__ . '/banner-contact' );
 
 	// Register Result
 	register_block_type( __DIR__ . '/results' );
@@ -24,4 +40,17 @@ function jda_register_acf_blocks() {
 	// Register Banner banner
 	register_block_type( __DIR__ . '/cta' );
 
+	// Register Header  block
+	register_block_type( __DIR__ . '/header' );
+
+	// Register Cabecera  block
+	register_block_type( __DIR__ . '/case-study' );
+
+	// Register FAQ block
+	register_block_type( __DIR__ . '/faq' );
+
+	// Register Goals block
+	register_block_type( __DIR__ . '/goals' );
+
 }
+
