cd9a8d6f9eb387cd381ebf9e111d6c90f897a96c
jandcreative <javi.diaz@jandcreative.com>
2023-11-02 12:43:12 +0100

    update contact block

--- header-contact/header-contact.php
+++ header-contact/header-contact.php
@@ -3,34 +3,33 @@
  * Banner Book a Call Block Template.
  */
 
- $anchor = '';
- if ( !empty( $block['anchor'] ) ):
-    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
- endif;
+$anchor = '';
+if (!empty($block['anchor'])):
+   $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+endif;
 
- $class_name = 'banner';
- if( !empty($block['className'] ) ):
-    $class_name .= ' ' . esc_attr( $block['className'] );
- endif;
+$class_name = 'banner';
+if (!empty($block['className'])):
+   $class_name .= ' ' . esc_attr($block['className']);
+endif;
 
-if(!empty($block['align'])):
+if (!empty($block['align'])):
    $class_name .= ' align' . esc_attr($block['align']);
 endif;
 
+// ACF Fields
+$title_contact = get_field('title_contact_header');
+$button_contact = get_field('button_contact_header');
+$link_contact = get_field('link_contact_header');
 ?>
 
-
 <section id="banner" class="hero_book">
-
-      <div class="hero">
-        <h1>Ready? Book a call</h1>
-
-		   <a class="button" ref="#">Get a call on the calendar</a>
-      </div>
+   <div class="hero">
+      <h1>
+         <?php echo $title_contact; ?>
+      </h1>
+      <a class="button" href="<?php echo $link_contact; ?>">
+         <?php echo $button_contact; ?>
+      </a>
+   </div>
 </section>
-
-
-
-
-
-
