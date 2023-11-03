542585f43f6c53eca8a84971e194cf29a7239db5
jandcreative <javi.diaz@jandcreative.com>
2023-11-03 12:15:10 +0100

    update import blcoks and categories

--- jda-acf-blocks.php
+++ jda-acf-blocks.php
@@ -6,85 +6,91 @@
   Version:      1.0
 */
 
-add_action( 'init', 'jda_register_acf_blocks', 5 );
+add_action('init', 'jda_register_acf_blocks', 5);
 
-function jda_register_acf_blocks() {
+function jda_register_acf_blocks()
+{
 
-	// Register Cabecera  block
-	register_block_type( __DIR__ . '/hero' );
+	// Register Header Template
+	register_block_type(__DIR__ . '/header-template');
 
-	// Register Cabecera  block
-	register_block_type( __DIR__ . '/cabecera' );
-
-	// Register Spacers block
-	register_block_type( __DIR__ . '/spacer' );
-
-	// Register Brands block
-	register_block_type( __DIR__ . '/brands' );
-
-	// Register Process block
-	register_block_type( __DIR__ . '/process' );
-
-	// Register Result
-	register_block_type( __DIR__ . '/alpha-process' );
+	// Register  Header Services
+	register_block_type(__DIR__ . '/header-services');
 
 	// Register Header Resources
-	register_block_type( __DIR__ . '/header-resources' );
-
-	// Register Result
-	register_block_type( __DIR__ . '/results' );
-
-	// Register Banner banner
-	register_block_type( __DIR__ . '/video' );
-
-	// Register Banner banner
-	register_block_type( __DIR__ . '/cta' );
-
-	// Register Header  block
-	register_block_type( __DIR__ . '/header' );
-
-	// Register Cabecera  block
-	register_block_type( __DIR__ . '/case-study' );
-
-	// Register FAQ block
-	register_block_type( __DIR__ . '/faq' );
-
-	// Register Goals block
-	register_block_type( __DIR__ . '/goals' );
-
-	// Register Grid Cases
-	register_block_type( __DIR__ . '/grid-cases' );
-
-	// Register Values
-	register_block_type( __DIR__ . '/values' );
-	
-	// Register Team
-	register_block_type( __DIR__ . '/team-slider' );
-		
-	// Register Testimonials
-	register_block_type( __DIR__ . '/testimonials' );
-	
-	// Register Header Result	
-	register_block_type( __DIR__ . '/header' );
+	register_block_type(__DIR__ . '/header-resources');
 
 	// Register Header About us
-	register_block_type( __DIR__ . '/header-about' );
+	register_block_type(__DIR__ . '/header-about');
 
 	// Register  Header Contact
-	register_block_type( __DIR__ . '/header-contact' );
+	register_block_type(__DIR__ . '/header-contact');
 
+	// Register Spacer
+	register_block_type(__DIR__ . '/spacer');
+
+	// Register Brands
+	register_block_type(__DIR__ . '/brands');
+
+	// Register Process
+	register_block_type(__DIR__ . '/process');
+
+	// Register Results
+	register_block_type(__DIR__ . '/results');
+
+	// Register Video
+	register_block_type(__DIR__ . '/video');
+
+	// Register CTA
+	register_block_type(__DIR__ . '/cta');
+
+	// Register Cabecera  block
+	register_block_type(__DIR__ . '/case-study');
+
+	// Register FAQ
+	register_block_type(__DIR__ . '/faq');
+
+	// Register Goals
+	register_block_type(__DIR__ . '/goals');
+
+	// Register Grid Cases
+	register_block_type(__DIR__ . '/grid-cases');
+
+	// Register Values
+	register_block_type(__DIR__ . '/values');
+
+	// Register Team
+	register_block_type(__DIR__ . '/team-slider');
+
+	// Register Testimonials
+	register_block_type(__DIR__ . '/testimonials');
 
 	// Register First Phase
-	register_block_type( __DIR__ . '/phase' );
+	register_block_type(__DIR__ . '/phase');
 
 	// Register Second Phase
-	register_block_type( __DIR__ . '/second_phase' );
+	register_block_type(__DIR__ . '/second_phase');
 
 	// Register Third Phase
-	register_block_type( __DIR__ . '/third_phase' );
+	register_block_type(__DIR__ . '/third_phase');
 
 	// Register Fourth Phase
-	register_block_type( __DIR__ . '/fourth_phase' );
+	register_block_type(__DIR__ . '/fourth_phase');
 
 }
 
+
+add_filter('block_categories_all', 'custom_block_category', 10, 2);
+function custom_block_category($categories)
+{
+
+	array_unshift(
+		$categories,
+		array(
+			'slug' => 'flyingcat_block_category',
+			'title' => 'FlyingCat'
+		)
+	);
+
+	return $categories;
+}
