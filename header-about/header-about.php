261efa8e18a8d9b4ab891f7990321751d7a484a6
jandcreative <javi.diaz@jandcreative.com>
2023-11-03 12:13:32 +0100

    update header about

--- header-about/header-about.php
+++ header-about/header-about.php
@@ -3,15 +3,15 @@
  * Header Resources.
  */
 
- $anchor = '';
- if ( !empty( $block['anchor'] ) ):
-    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
- endif;
+$anchor = '';
+if (!empty($block['anchor'])):
+   $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+endif;
 
- $class_name = 'hero';
- if( !empty($block['className'] ) ):
-    $class_name .= ' ' . esc_attr( $block['className'] );
- endif;
+$class_name = 'hero';
+if (!empty($block['className'])):
+   $class_name .= ' ' . esc_attr($block['className']);
+endif;
 
 // ACF Fields
 $image_about = get_field('hero_image_about');
@@ -20,19 +20,19 @@ $title_about = get_field('title_bubbles_about');
 
 <section id="banner" class="hero_about" style="background-color:#DFDFA8">
 
-<div class="hero">
+   <div class="hero">
       <div class="pictures">
-         <img src="<?php echo $image_about;?>">
+         <img src="<?php echo $image_about; ?>">
       </div>
 
-      <?php if( !empty( $title_about ) ): ?>
-      
-      <div class="bubble">
-            <h1><?php echo $title_about;?></h1>         
-      </div>
+      <?php if (!empty($title_about)): ?>
+
+         <div class="bubble">
+            <h1>
+               <?php echo $title_about; ?>
+            </h1>
+         </div>
 
       <?php endif; ?>
-</div>
+   </div>
 </section>
-
-
