2636a73e22696ffd21e7016e4e2c1fe767c3b29a
jandcreative <javi.diaz@jandcreative.com>
2023-11-09 13:47:12 +0100

    update bubble content block - css

--- bubble-content/bubble-content.php
+++ bubble-content/bubble-content.php
@@ -9,17 +9,19 @@ if (!empty($block['className'])):
 endif;
 
 // ACF Fields
-$cat_case = get_field('img_cat_case_study');
-$html_bubble_content = get_field('html_content_case_study');
+$cat_case = get_field('img_cat_bubble_content');
+$html_bubble_content = get_field('html_content_bubble_content');
 ?>
 
-<div class="<?php echo $class_name; ?>">
+<?php if (!empty($html_bubble_content)): ?>
+   <div class="<?php echo $class_name; ?>">
 
-   <div class="item-testimonial">
-      <div class="cut-cat"><img src="<?php echo $cat_case; ?>"></div>
-      <div class="content">
+      <div class="item-testimonial">
+         <div class="cut-cat"><img src="<?php echo $cat_case; ?>"></div>
+         <div class="content">
 
-         <?php echo $html_content_case; ?>
+            <?php echo $html_bubble_content; ?>
 
+         </div>
       </div>
-   </div>
+   <?php endif ?>
