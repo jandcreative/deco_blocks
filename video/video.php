f96d64fe952467dbfd72bf10efce56d01a4a87ec
jandcreative <javi.diaz@jandcreative.com>
2023-11-14 18:02:45 +0100

    update hero template mark

--- video/video.php
+++ video/video.php
@@ -3,18 +3,18 @@
  * Video Block Template.
  */
 
- $anchor = '';
- if ( !empty( $block['anchor'] ) ):
-    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
- endif;
+$anchor = '';
+if (!empty($block['anchor'])):
+	$anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+endif;
 
- $class_name = 'video';
- if( !empty($block['className'] ) ):
-    $class_name .= ' ' . esc_attr( $block['className'] );
- endif;
+$class_name = 'video';
+if (!empty($block['className'])):
+	$class_name .= ' ' . esc_attr($block['className']);
+endif;
 
-if(!empty($block['align'])):
-   $class_name .= ' align' . esc_attr($block['align']);
+if (!empty($block['align'])):
+	$class_name .= ' align' . esc_attr($block['align']);
 endif;
 
 
@@ -25,14 +25,13 @@ $image_video = get_field('image_video');
 $html = get_field('html_video');
 ?>
 
-<div class="container-<?php echo $class_name;?>">
-	<img src="<?php echo $image_cat;?>">
-	<h2><?php echo $title;?></h2>
+<div class="container-<?php echo $class_name; ?> reveal-opacity">
+	<img src="<?php echo $image_cat; ?>">
+	<h2>
+		<?php echo $title; ?>
+	</h2>
 	<div class="video-thumb">
-	<a class="wp-video-popup" href="#"><img src="<?php echo $image_video;?>"></a>
-   <?php echo $html;?>
+		<a class="wp-video-popup" href="#"><img src="<?php echo $image_video; ?>"></a>
+		<?php echo $html; ?>
 	</div>
 </div>
-					
-
-
