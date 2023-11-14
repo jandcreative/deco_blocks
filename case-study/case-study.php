63e5a59ea32181ed20c5d7ab9841191bf99f2e4f
jandcreative <javi.diaz@jandcreative.com>
2023-11-14 18:05:22 +0100

    update case study block - animation css

--- case-study/case-study.php
+++ case-study/case-study.php
@@ -1,17 +1,17 @@
 <?php
- /**
+/**
  * Case Study Block.
  */
 
- $anchor = '';
- if ( !empty( $block['anchor'] ) ):
-    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
- endif;
+$anchor = '';
+if (!empty($block['anchor'])):
+   $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+endif;
 
- $class_name = 'case-study';
- if( !empty($block['className'] ) ):
-    $class_name .= ' ' . esc_attr( $block['className'] );
- endif;
+$class_name = 'case-study';
+if (!empty($block['className'])):
+   $class_name .= ' ' . esc_attr($block['className']);
+endif;
 
 // ACF Fields
 /* Brands */
@@ -30,45 +30,53 @@ $name_case = get_field('name_case_study');
 $position_case = get_field('position_case_study');
 ?>
 
-<section <?php echo $anchor;?>class="<?php echo $class_name;?>">
-  
-   <div class="thumb" style="background-image: url('<?php echo $background_case;?>');">
+<section <?php echo $anchor; ?>class="<?php echo $class_name; ?>">
 
-         <div class="box-brand">
-            <div class="brand">
-               <img src="<?php echo $logo_case;?>">
-            </div>
+   <div class="thumb" style="background-image: url('<?php echo $background_case; ?>');">
 
-            <div class="data-content">
-               <span class="number-help"><?php echo $number_case;?></span>
-               <h2><?php echo $title_case;?></h2>
-            </div>
-            <button><a style="color:#fff" href="<?php echo $link_case;?>" target="_blank">
-               <?php echo $button_case;?>
-					</a>
-            </button>
+      <div class="box-brand" data-aos="fade-right" data-aos-duration="1000">
+         <div class="brand">
+            <img src="<?php echo $logo_case; ?>">
          </div>
+
+         <div class="data-content">
+            <span class="number-help">
+               <?php echo $number_case; ?>
+            </span>
+            <h2>
+               <?php echo $title_case; ?>
+            </h2>
+         </div>
+         <button><a style="color:#fff" href="<?php echo $link_case; ?>" target="_blank">
+               <?php echo $button_case; ?>
+            </a>
+         </button>
+      </div>
 
    </div>
 
    <div class="testimonial">
 
-      <div class="item-testimonial">
-         <div class="cut-cat"><img src="<?php echo $cat_case;?>"></div>
+      <div class="item-testimonial" data-aos="fade-left" data-aos-duration="1000">
+         <div class="cut-cat"><img src="<?php echo $cat_case; ?>"></div>
          <div class="content">
 
-            <?php echo $html_content_case;?>
+            <?php echo $html_content_case; ?>
 
          </div>
 
          <div class="info-profile">
             <div class="avatar">
-               <img src="<?php echo $img_avatar_case;?>">
+               <img src="<?php echo $img_avatar_case; ?>">
             </div>
             <div class="txt">
-               <span class="name"><?php echo $name_case;?></span>   
-               <span class="position"><?php echo $position_case;?></span>
+               <span class="name">
+                  <?php echo $name_case; ?>
+               </span>
+               <span class="position">
+                  <?php echo $position_case; ?>
+               </span>
             </div>
          </div>
       </div>
- </section>
+</section>
