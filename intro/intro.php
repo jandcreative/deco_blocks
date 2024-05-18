556e67aa9d5d357c4ef4d12f82fab79902642065
jandcreative <javi.diaz@jandcreative.com>
2024-05-18 12:51:43 +0200

    update intro block

--- intro/intro.php
+++ intro/intro.php
@@ -16,20 +16,32 @@ endif;
 // ACF Fields
 $title_intro = get_field('title_intro');
 $txt_intro = get_field('text_intro');
+
+$type_title = get_field('type_title');
 ?>
 
 <div <?php echo $anchor; ?> class="<?php echo $class_name; ?>">
 
-	<?php if (!empty($title_intro)): ?>
+<?php if (!empty($title_intro)): ?>
 
+		<?php if ($type_title == 'title_01'): ?>
+		<h1>
+			<?php echo $title_intro; ?>
+		</h1>
+		<?php endif; ?>
+
+		<?php if ($type_title == 'title_02'): ?>
 		<h2>
 			<?php echo $title_intro; ?>
 		</h2>
+		<?php endif; ?>
 
 		<?php if (!empty($txt_intro)): ?>
+			
 			<?php echo $txt_intro; ?>
+			
 		<?php endif; ?>
 		
-	<?php endif; ?>
+<?php endif; ?>
 
 		</div>
