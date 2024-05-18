a540ba70b484c6605a9b3e8e02862a233fcdf7c4
jandcreative <javi.diaz@jandcreative.com>
2024-05-18 12:43:25 +0200

    update accordion block

--- acordeon/acordeon.php
+++ acordeon/acordeon.php
@@ -17,23 +17,22 @@ endif;
 $accordeon_num = get_field('acordeon_numero');
 ?>
 
-
 <section <?php echo $anchor; ?> id="accordion">
 
     <div class="<?php echo $class_name; ?>">
 
         <div class="container-accordion">
 
-            <div class="items-accordion-<?php echo $accordeon_num; ?>">
+            <div class="items-accordion-01">
                 <?php while (have_rows('list_accordion')):
                     the_row(); ?>
                     <div class="question-box">
 
                         <div class="item-accordion">
-                            <h2>
+                            <h3>
                                 <?php the_sub_field('title_section'); ?>
-                            </h2>
-                            <span class="plus"> <img src="<?php the_field('icon_arrow', 'options'); ?>"> </span>
+                            </h3>
+                            <span class="plus"><img src="<?php the_field('icon_arrow', 'options'); ?>"></span>
                         </div>
 
                         <div class="description">
