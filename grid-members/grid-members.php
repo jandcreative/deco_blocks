c1f504d70ab70b0aec447a0bfdff1dd942fff8a2
jandcreative <javi.diaz@jandcreative.com>
2024-05-19 16:34:59 +0200

    update grid members  - block

--- grid-members/grid-members.php
+++ grid-members/grid-members.php
@@ -12,21 +12,22 @@ $class_name = 'grid-members';
 if (!empty($block['className'])):
 	$class_name .= ' ' . esc_attr($block['className']);
 endif;
-?>
-	<div class="list-<?php echo $class_name; ?>">
 
-		<?php while (have_rows('list_members')): the_row(); ?>
-		
+if (!empty($block['align'])):
+	$class_name .= ' align' . esc_attr($block['align']);
+endif;
+
+?>
+	<div class="list-grid-members">
+		<?php while (have_rows('list_members_02')): the_row(); ?>
 		<div class="item" style="border-radius: 15px">
-			<img src="<?php the_sub_field('img_member'); ?>">
+			<img src="<?php the_sub_field('img_members');?>">
 			<div class="content">
 				<div class="text">
-					<h3><?php the_sub_field('name_member'); ?></h3>
-					<p><?php the_sub_field('position_member'); ?></p>
+					<h3><?php the_sub_field('name_members'); ?></h3>
+					<p><?php the_sub_field('position_members'); ?></p>
 				</div>
 			</div>
 		</div>
-
 		<?php endwhile; ?>
-
 	</div>
