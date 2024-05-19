f2153ef3a58dd6c0728ed3d7d010b30b512bbf22
jandcreative <javi.diaz@jandcreative.com>
2024-05-19 16:36:48 +0200

    update paths - blocks

--- decotherapy-acf-blocks.php
+++ decotherapy-acf-blocks.php
@@ -6,6 +6,10 @@ add_action('init', 'decotherapy_register_acf_blocks', 5);
 
 function decotherapy_register_acf_blocks()
 {
+
+	// Equipo
+	register_block_type(__DIR__ . '/equipo');
+
 	// Register Hero Home block
 	register_block_type(__DIR__ . '/hero-home');
 
@@ -45,6 +49,9 @@ function decotherapy_register_acf_blocks()
 	// Register Banner Imagen Texto
 	register_block_type(__DIR__ . '/banner-imagen-texto');
 
+	// Imagenes Proyectos
+	register_block_type(__DIR__ . '/imagenes-proyectos');
+
 	// Proceso
 	register_block_type(__DIR__ . '/proceso');
 
@@ -54,21 +61,27 @@ function decotherapy_register_acf_blocks()
 	// Decopacks
 	register_block_type(__DIR__ . '/decopacks');
 
-	// Decopacks
-	register_block_type(__DIR__ . '/decopacks-base');
+	// Decopacks Pymes
+	register_block_type(__DIR__ . '/decopacks-pymes');
 
 	// Grid Banners
 	register_block_type(__DIR__ . '/grid-banners');
 
-	// Grid Banners
-	register_block_type(__DIR__ . '/grid-members');
+	/* 
+		// Grid Members
+		register_block_type(__DIR__ . '/grid-members'); */
 
-	// Grid Banners
+	// Maps
 	register_block_type(__DIR__ . '/maps');
 
+	// Maps Mad
+	register_block_type(__DIR__ . '/maps-mad');
+	
+	   
+	// Register CTA
+	register_block_type(__DIR__ . '/cta');
 
-
-	/*   // Register Spacer block
-	register_block_type(__DIR__ . '/spacer'); */
+	// Register Spacer block
+	register_block_type(__DIR__ . '/spacer');
 }
 
